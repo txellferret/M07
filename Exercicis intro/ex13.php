6561e2c2f33c7c47aa8cc4f90ebf8dcba58ab0cd
txellfe <txellfe@gmail.com>
2020-10-22 13:17:56 +0200

    exercicis seq

--- Exercicis intro/ex13.php
+++ Exercicis intro/ex13.php
@@ -1,7 +1,78 @@
 <?php
-//
+//Programa que donada la seqüència d'aminoàcids d'una proteïna, 
+//mostra una seqüència de nucleòtids que la codifiqui.
+
+$divStyleRNA='none'; // hide div
+
+define("AA", array("A", "R", "N", "D", "C", "Q", "E", "G", "H", "I", "L", "K", "M", "F", "P", "S", "T", "W", "Y", "V")); 
+$nucleotidsRNAAA = array ("F" =>"UUU/UUC","L" =>"UUA/UUG/CU*", "S" =>"UC*",
+"Y" => "UAU/UAC", "C" => "UGU/UGC", "W" => "UGG", 
+"P" => "CC*", "H" =>"CAU/CAC", "Q" => "CAA/CAG", "R" =>"CG*/AGA/AGG" , 
+"I" => "AUU/AUC/AUA", "M" =>"AUG" , "T" =>"AC*" , "N" =>"AAU/AAC","K" => "AAA/AAG", 
+"S" =>"AGU/AGC", "V" =>"GU*", "A" => "GC*", "D" =>"GAU/GAC",
+"E" => "GAA/GAG", "G" =>"GG*");
+
+if (!is_null(filter_input(INPUT_GET,'analize'))) { //if button is clicked
+
+    $inputSeq = strtoupper(filter_input(INPUT_GET, "seq"));
+    if (checkValidSequence($inputSeq, AA)) {
+        echo "valid";
+        $aminoacids = splitsSeqRNA ($inputSeq,1);
+        print_r($aminoacids);
+        $seqRNA = convertToNucleotids ($aminoacids);
+    
+    }else $errors = "Invalid sequence";
+
+    $divStyleRNA='block'; //show div
 
 
+}
+function convertToNucleotids (array $seq) :string {
+    $chainNucleotids = "";
+    global $nucleotidsRNAAA;
+    for ($i=0; $i < count($seq); $i++) { 
+        $aa = $seq[$i];
+        
+        if(array_key_exists($aa, $nucleotidsRNAAA)){
+            $chainNucleotids .= $nucleotidsRNAAA[$codon]; 
+        
+       
+        } 
+    }
+    return $chainNucleotids;
+}
+
+/**
+ * Validates a sequences
+ * @param seq to validate
+ * @param validCahrs acceptable chars on the sequence
+ * @return true if sequence is valid, false otherwise
+ */
+function checkValidSequence (string $seq, array $validChars) : bool {
+    $valid = true; 
+    for ($i=0; $i<strlen($seq); $i++) {
+        if (!in_array(strtoupper($seq[$i]), $validChars)) {
+            $valid = false; 
+            break;
+        }  
+    }
+    return $valid;
+}
+/**
+ * Splits a string into an array
+ * @param string to split
+ * @return the array of strings splitted
+ */
+function splitsSeqRNA (string $seq, int $j) : array {
+
+    $codons = array();
+    for ($i=0; $i <= strlen($seq)-$j; $i +=$j) { 
+        $prova = substr($seq, $i, $j);
+        array_push($codons, $prova);
+        
+    }
+    return $codons;
+}
 ?>
 
 
@@ -13,6 +84,22 @@
     <title>Document</title>
 </head>
 <body>
+    <form name="analize-form" method="get" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+        <fieldset>
+        <legend>Analize sequence</legend>
+            <div>
+                <label for="seq">Sequence </label>
+                <input type="text" name="seqAA" id ="seqAA" value="<?php  print($inputSeq); ?>"></input>
+            </div>
+
+            <div>
+                <button type="submit" name="analize" value="analize">Analize</button>
+            </div>
+            <div id="dna" style="display: <?= $divStyleRNA?>">
+                <p> <?php echo $seqRNA?> </p>
+                <p> <?php echo "Errors: ". $errors?> </p>
+            </div>
+            
     
 </body>
 </html>
---
+++ Exercicis intro/ex14.php
@@ -0,0 +1,141 @@
+<?php
+//Programa que donada una seqüència de nucleòtids, 
+//indiqui la seqüència d'aminoàcids en què s'expressa (amb lletra i nom).
+
+
+$divStyleAA='none'; // hide div
+define("RNACHARS", array("A", "U", "G", "C"));
+$errors = "0";
+
+$nameAA = array ("F" => "Phenylalanine", "L" => "Leucine", "I" => "Isoleucine", 
+"M" => "Methionine", "V" => "Valine", "S" => "Serine", "P" => "Proline", "T" => "Threonine", 
+"A" => "Alanine", "Y" => "Tyrosine", "H" => "Histidine", "Q" => "Glutamine", "N" => "Aspargine", 
+"K" => "Lysine", "D" => "Aspartic acid", "E" => "Glutamic acid", "C" => "Cysteine", "W" => "Tryptophan", 
+"R" => "Arginine", "G" => "Glycine");
+
+$nucleotidsAA = array ("UUU" => "F","UUC" => "F","UUA" => "L", "UUG" => "L", "UC*" => "S",
+"UAU" => "Y", "UAC" => "Y", "UGU" => "C", "UGC" => "C", "UGG" => "W", 
+"CU*" => "L", "CC*" => "P", "CAU" => "H", "CAC" => "H", "CAA" => "Q", "CAG" => "Q", "CG*" => "R", 
+"AUU" => "I", "AUC" => "I", "AUA" => "I", "AUG" => "M", "AC" => "T", "AAU" => "N", "AAC" => "N", "AAA" => "K","AAG" => "K", 
+"AGU" => "S", "AGC" => "S", "AGA" => "R", "AGG" => "R", "GU*" => "V","GC*" => "A", "GAU" => "D","GAC" => "D",
+"GAA" => "E","GAG" => "E", "GG*" => "G");
+
+
+if (!is_null(filter_input(INPUT_GET,'analize'))) { //if button is clicked
+    $inputSeq = strtoupper(filter_input(INPUT_GET, "seq"));
+    if (checkValidSequence($inputSeq, RNACHARS)) {
+        $arrayCodons = splitsSeqRNA ($inputSeq,3);
+        $AA = convertToAA ($arrayCodons);
+
+        //converts to their AA names
+        $AAname = convertNameAA($AA);
+
+
+    } else $errors = "Invalid sequence";
+
+    $divStyleAA='block'; //show div
+}
+
+/**
+ * Converts a given aminoacid symbol string chain to their complete name
+ * @param string of aminoacids symbols
+ * @return the string with the complete name of each AA
+ */
+function convertNameAA (string $aminoacids) :string{
+    $arrayAA = splitsSeqRNA ($aminoacids,1);
+    $chainAAname = "";
+    global $nameAA;
+    for ($i=0; $i < count($arrayAA); $i++) { 
+        $name = $arrayAA[$i];
+        
+        if(array_key_exists($name, $nameAA)){
+            $chainAAname .= $nameAA[$name]." "; 
+        }
+    }
+    return $chainAAname;
+}
+
+/**
+ * Validates a sequences
+ * @param seq to validate
+ * @param validCahrs acceptable chars on the sequence
+ * @return true if sequence is valid, false otherwise
+ */
+function checkValidSequence (string $seq, array $validChars) : bool {
+    $valid = true; 
+    for ($i=0; $i<strlen($seq); $i++) {
+        if (!in_array(strtoupper($seq[$i]), $validChars)) {
+            $valid = false; 
+            break;
+        }  
+    }
+    return $valid;
+}
+/**
+ * Converts a given array of nucleotids to their correspondig AAs
+ * @param array of codons
+ * @return the sequence of AAs
+ */
+function convertToAA (array $seq) :string {
+    $chainAA = "";
+    global $nucleotidsAA;
+    for ($i=0; $i < count($seq); $i++) { 
+        $codon = $seq[$i];
+        
+        if(array_key_exists($codon, $nucleotidsAA)){
+            $chainAA .= $nucleotidsAA[$codon]; 
+        
+        //AA codificants amb dos nucleotids
+        } elseif (array_key_exists(substr($codon, 0, 2)."*",$nucleotidsAA )) {
+            $chainAA .= $nucleotidsAA[substr($codon, 0, 2)."*"];
+        } 
+    }
+    return $chainAA;
+}
+
+/**
+ * Splits a string into an array
+ * @param string to split
+ * @return the array of strings splitted
+ */
+function splitsSeqRNA (string $seq, int $j) : array {
+
+    $codons = array();
+    for ($i=0; $i <= strlen($seq)-$j; $i +=$j) { 
+        $prova = substr($seq, $i, $j);
+        array_push($codons, $prova);
+        
+    }
+    return $codons;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form name="analize-form" method="get" action="<?php echo htmlentities($_SERVER['PHP_SELF']); ?>">
+        <fieldset>
+        <legend>Analize sequence</legend>
+            <div>
+                <label for="seq"><b>RNA</b> sequence </label>
+                <input type="text" name="seq" id ="seq" value="<?php  print($inputSeq); ?>"></input>
+            </div>
+            <div>
+                <button type="submit" name="analize" value="analize">Analize</button>
+            </div>
+            <div id="aa" style="display: <?= $divStyleAA?>">
+                <p><?php echo "Aminoacid sequence: ".$AA?> </p>
+                <p><?php echo "Aminoacid sequence name: ".$AAname?> </p>
+                <p> <?php echo "Errors: " . $errors?> </p>
+            </div>
+        </fieldset>
+    </form>
+</body>
+</html>
